cf12f1ccfd3982c676a26e04e863e44368b8a8b9
John Ellis <john@seriouslyawesome.com>
2015-01-25 21:42:37 -0600

    Create issues from pending specs.
    It works, baby!

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  describe 'Active Record', pending: true do
-    it 'should do thing' do
+  describe '.first_name', pending: true do
+    it 'should output the user\'s name' do
+      expect(true).to eq false
+    end
+
+    it 'should not return someone else\'s name' do
+      expect(true).to eq false
+    end
+  end
+
+  describe '.last_name' do
+    it 'should output the user\'s last name'
+    it 'should create an issue', pending: true do
       expect(true).to eq false
     end
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,15 +11,26 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
+  config.run_all_when_everything_filtered = true
 
   config.after(:suite) do
     #get github issues
+    repo = 'freddyrangel/snitch-example'
     github = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
-    issues = github.list_issues('freddyrangel/snitch-example')
+    issues = github.list_issues(repo, state: 'open')
     issue_titles = issues.map(&:title)
 
     #get pending examples
     examples = config.instance_variable_get(:@reporter)
-    pending examples = examples.pending_examples
+    pendings = examples.pending_examples
+
+    pendings.each do |example|
+      title = example.full_description
+      body = "#{example.location}\n\nThis issue was created automatically by rspec-snitch."
+
+      unless issue_titles.include? title
+        github.create_issue(repo, title, body) if example.pending?
+      end
+    end
   end
 end
